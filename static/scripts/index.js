11eb4e1fa93038cef5ba1b87377bbf5d344f3e12
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-07-30 02:34:18 -0300

    Add simple "easter egg" to the legal disclaimer

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,6 +4,25 @@ import sidebar from './sidebar.js'
 $(async () => {
   const notes = await requests.fetchNotes(false)
   sidebar.showNotes(notes)
-
+  
   sidebar.initSearchBar()
+  initEasterEgg()
 })
+
+function initEasterEgg() {
+  const $el = $('.legal-disclaimer')
+  let clicks = []
+
+  $el.on('click', () => {
+    console.log(clicks.length)
+    const now = Date.now()
+    clicks.push(now)
+
+    if (clicks.length > 3) clicks.shift()
+
+    if (clicks.length === 3 && (clicks[2] - clicks[0]) <= 1500) {
+      window.open('https://www.magazineluiza.com.br', '_blank')
+      clicks = []
+    }
+  })
+}
