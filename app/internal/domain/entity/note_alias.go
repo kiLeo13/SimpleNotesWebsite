66359241fb5c791117bf377c75e979a039898f3c
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-08-17 23:16:37 -0300

    Move backend to its own repository

--- app/internal/domain/entity/note_alias.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-// NoteAlias is meant to be immutable
-type NoteAlias struct {
-	ID        int    `gorm:"primaryKey"`
-	NoteID    int    `gorm:"not null"`
-	Value     string `gorm:"not null"`
-	CreatedAt int64  `gorm:"not null"`
-}
