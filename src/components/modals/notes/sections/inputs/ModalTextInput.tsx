0cea1581f5cc392b17617a6e3079609b6316ebe5
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-06 11:13:03 -0300

    Add props support to autocomplete option

--- src/components/modals/notes/sections/inputs/ModalTextInput.tsx
+++ src/components/modals/notes/sections/inputs/ModalTextInput.tsx
@@ -1,19 +1,20 @@
-import type { JSX } from "react"
+import type { HTMLInputAutoCompleteAttribute, JSX } from "react"
+import { useController, useFormContext, type FieldValues, type Path } from "react-hook-form"
 
 import clsx from "clsx"
-
-import { useController, useFormContext, type FieldValues, type Path } from "react-hook-form"
 
 import styles from "./ModalInputs.module.css"
 
 type ModalTextInputProps<T extends FieldValues> = {
   name: Path<T>;
   placeholder?: string
+  autoComplete?: HTMLInputAutoCompleteAttribute
 }
 
 export function ModalTextInput<T extends FieldValues>({
   name,
-  placeholder
+  placeholder,
+  autoComplete
 }: ModalTextInputProps<T>): JSX.Element {
   const { control } = useFormContext<T>()
   const { field, fieldState } = useController<T>({ name, control })
@@ -25,6 +26,7 @@ export function ModalTextInput<T extends FieldValues>({
         {...field}
         className={clsx(styles.input, errorMessage && styles.invalid)}
         placeholder={placeholder}
+        autoComplete={autoComplete}
       />
 
       {errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
