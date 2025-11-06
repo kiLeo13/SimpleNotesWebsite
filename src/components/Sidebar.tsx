8c96d008b1b66365c84fdd03583c00bcbfb9ca7a
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-06 02:39:48 -0300

    Add support to note content display

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import type { NoteResponseData } from "../types/api/notes"
+import type { FullNoteResponseData, NoteResponseData } from "../types/api/notes"
 import type { UserResponseData } from "../types/api/users"
-import { useCallback, useEffect, useRef, useState, type ChangeEventHandler, type JSX, type KeyboardEventHandler } from "react"
+import { useCallback, useEffect, useRef, useState, type ChangeEventHandler, type JSX, type KeyboardEventHandler, type MouseEventHandler } from "react"
 
 import { noteService } from "../services/noteService"
 import { SidebarNote } from "./notes/SidebarNote"
@@ -11,10 +11,22 @@ import styles from "./Sidebar.module.css"
 type SidebarProps = {
   selfUser: UserResponseData | null
   notes: NoteResponseData[]
+  showUploadModal: boolean
+  shownNote: NoteResponseData | null,
   setNotes: (notes: NoteResponseData[]) => void
+  setShowUploadModal: (show: boolean) => void
+  setShownNote: (note: FullNoteResponseData) => void
 }
 
-export function Sidebar({ selfUser, notes, setNotes }: SidebarProps): JSX.Element {
+export function Sidebar({
+  selfUser,
+  notes,
+  showUploadModal,
+  setNotes,
+  shownNote,
+  setShowUploadModal,
+  setShownNote
+}: SidebarProps): JSX.Element {
   const [isLoading, setIsLoading] = useState(false)
   const [search, setSearch] = useState('')
   const searchRef = useRef<HTMLInputElement>(null)
@@ -33,14 +45,34 @@ export function Sidebar({ selfUser, notes, setNotes }: SidebarProps): JSX.Elemen
   }, [setNotes])
 
   const handleSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const val = e.target.value
-    setSearch(val)
+    setSearch(e.target.value)
   }
 
   const handleKeyboard: KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key.toLowerCase() === "escape") {
       searchRef?.current?.blur()
     }
+  }
+
+  const handleOpenNote = async (n: NoteResponseData) => {
+    // Are trying to open the same note?
+    if (shownNote?.id === n.id) return
+
+    if (n.note_type === 'REFERENCE') {
+      setShownNote(n)
+    } else {
+      const resp = await noteService.fetchNote(n.id) 
+      
+      if (resp.success) {
+        setShownNote(resp.data)
+      } else {
+        alert(`Erro: ${JSON.stringify(resp.errors, null, 2)}`)
+      }
+    }
+  }
+
+  const handleShowUpload: MouseEventHandler<HTMLButtonElement> = () => {
+    setShowUploadModal(!showUploadModal)
   }
 
   useEffect(() => {
@@ -56,7 +88,6 @@ export function Sidebar({ selfUser, notes, setNotes }: SidebarProps): JSX.Elemen
         loadNotes()
       }
     }
-
     window.addEventListener("keydown", handleGlobalKeydown)
     return () => window.removeEventListener("keydown", handleGlobalKeydown)
   })
@@ -89,14 +120,14 @@ export function Sidebar({ selfUser, notes, setNotes }: SidebarProps): JSX.Elemen
 
         {!isLoading && (
           filteredNotes.map((n) => {
-            return <SidebarNote key={n.id} name={n.name} />
+            return <SidebarNote onClick={() => handleOpenNote(n)} key={n.id} name={n.name} />
           })
         )}
       </div>
       <div className={styles.menuFooterControls}>
         <div className={styles.sidebarPfp}>L</div>
         {selfUser?.isAdmin && (
-          <button className={styles.footerControlButton}>
+          <button onClick={handleShowUpload} className={styles.footerControlButton}>
             <FaCloudUploadAlt size={"0.75em"} />
           </button>
         )}
--- src/pages/mainpage/MainPage.tsx
+++ src/pages/mainpage/MainPage.tsx
@@ -1,15 +1,21 @@
-import type { NoteResponseData } from "../../types/api/notes"
+import type { FullNoteResponseData, NoteResponseData } from "../../types/api/notes"
 import type { UserResponseData } from "../../types/api/users"
 import { useEffect, useState, type JSX } from "react"
 
 import { userService } from "../../services/userService"
 import { Sidebar } from "../../components/Sidebar"
+import { CreateNoteModalForm } from "../../components/modals/notes/CreateNoteModalForm"
+import { DarkWrapper } from "../../components/DarkWrapper"
 import { APP_NAME } from "../../App"
+import { EmptyDisplay } from "../../components/board/EmptyDisplay"
+import { ContentBoard } from "../../components/board/ContentBoard"
 
 import styles from "./MainPage.module.css"
 
 export function MainPage(): JSX.Element {
   const [notes, setNotes] = useState<NoteResponseData[]>([])
+  const [showUploadModal, setShowUploadModal] = useState(false)
+  const [shownNote, setShownNote] = useState<FullNoteResponseData | null>(null)
   const [selfUser, setSelfUser] = useState<UserResponseData | null>(null)
 
   useEffect(() => {
@@ -23,19 +29,54 @@ export function MainPage(): JSX.Element {
     loadSelfUser()
   }, [])
 
+  useEffect(() => {
+    const handleGlobalClose = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === 'escape') {
+        const target = e.target as HTMLElement
+      
+        // We don't want to close our note if we are typing
+        if (isInput(target)) return
+
+        setShownNote(null)
+      }
+    }
+    window.addEventListener('keydown', handleGlobalClose)
+    return () => window.removeEventListener('keydown', handleGlobalClose)
+  }, [])
+
   return (
     <>
       <title>{`${APP_NAME} - Anotações`}</title>
 
       <div className={styles.container}>
-        <Sidebar selfUser={selfUser} notes={notes} setNotes={setNotes} />
+        <Sidebar
+          selfUser={selfUser}
+          notes={notes}
+          showUploadModal={showUploadModal}
+          shownNote={shownNote}
+          setNotes={setNotes}
+          setShowUploadModal={setShowUploadModal}
+          setShownNote={setShownNote}
+        />
+
+        {showUploadModal && (
+          <DarkWrapper>
+            <CreateNoteModalForm setShowUploadModal={setShowUploadModal} />
+          </DarkWrapper>
+        )}
 
         <main className={styles.mainContent}>
-          <div className={styles.emptyBox}>
-            <span>:/</span>
-          </div>
+          {!shownNote ? (
+            <EmptyDisplay />
+          ) : (
+            <ContentBoard note={shownNote} />
+          )}
         </main>
       </div>
     </>
   )
+}
+
+function isInput(el: HTMLElement): boolean {
+  return el.tagName === 'INPUT' || el.tagName === 'TEXTAREA'
 }
