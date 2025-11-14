df909f37f8b0a0a315243c1c9c7544ee9e75efd6
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-14 02:38:47 -0300

    Add an optional `className` prop to `ModalSection` component

--- src/components/modals/notes/shared/sections/ModalSection.tsx
+++ src/components/modals/notes/shared/sections/ModalSection.tsx
@@ -1,17 +1,20 @@
 import type { ComponentProps, JSX } from "react"
 import type { ModalLabel } from "./ModalLabel"
 
+import clsx from "clsx"
+
 import styles from "./ModalSection.module.css"
 
 type ModalSectionProps = ComponentProps<"div"> & {
   label: React.ReactElement<typeof ModalLabel>
   input: React.ReactElement
   style?: React.CSSProperties
+  className?: string
 }
 
-export function ModalSection({ label, input, style }: ModalSectionProps): JSX.Element {
+export function ModalSection({ label, input, style, className }: ModalSectionProps): JSX.Element {
   return (
-    <div className={styles.section} style={style}>
+    <div className={clsx(styles.section, className)} style={style}>
       {label}
 
       <div className={styles.inputWrapper}>
