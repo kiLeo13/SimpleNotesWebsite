dcd74580929b356ebe8ccc2a7975abeeb268cedf
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-17 01:34:32 -0300

    Add some safeguards at `ModalFooter` component

--- src/components/modals/notes/updates/ModalFooter.tsx
+++ src/components/modals/notes/updates/ModalFooter.tsx
@@ -5,21 +5,26 @@ import { FaTrashAlt } from "react-icons/fa"
 import styles from "./ModalFooter.module.css"
 
 type ModalFooterProps = {
+  /**
+   * This property tracks whether the note is already in memory (exists).
+   * If this value is `false`, then we are likely still waiting for a server response.
+   */
+  exists: boolean
   setShowDelete: (show: boolean) => void
 }
 
-export function ModalFooter({ setShowDelete }: ModalFooterProps): JSX.Element {
+export function ModalFooter({ exists, setShowDelete }: ModalFooterProps): JSX.Element {
   const handleDeleteClick = () => {
     setShowDelete(true)
   }
 
   return (
     <footer className={styles.footer} onClick={handleDeleteClick}>
-      <button className={styles.deleteButton}>
+      <button disabled={!exists} className={styles.deleteButton}>
         <FaTrashAlt size={"1.1em"} color="rgba(102, 34, 34, 1)" />
       </button>
       
-      <button type="submit" className={styles.saveButton}>Salvar</button>
+      <button disabled={!exists} type="submit" className={styles.saveButton}>Salvar</button>
     </footer>
   )
 }
