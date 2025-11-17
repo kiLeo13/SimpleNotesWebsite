8f015930508196a4dcc268807fc3dcede2a71be2
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-17 01:19:04 -0300

    Split note update modal and form into different components

---
+++ src/components/modals/notes/updates/UpdateNoteForm.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState, type JSX } from "react"
+import type { FullNoteResponseData } from "../../../../types/api/notes"
+import type { UserResponseData } from "../../../../types/api/users"
+import type { UpdateNoteFormFields } from "../../../../types/forms/notes"
+import type { UseFormHandleSubmit } from "react-hook-form"
+
+import { userService } from "../../../../services/userService"
+import { ModalSection } from "../shared/sections/ModalSection"
+import { ModalActionRow } from "../shared/sections/ModalActionRow"
+import { ModalLabel } from "../shared/sections/ModalLabel"
+import { PiCrownFill } from "react-icons/pi"
+import { BaseModalTextInput } from "../shared/inputs/BaseModalTextInput"
+import { FaCalendarAlt } from "react-icons/fa"
+import { formatLocalTimestamp } from "../../../../utils/utils"
+import { ModalFooter } from "./ModalFooter"
+import { ModalArrayInput } from "../shared/inputs/ModalArrayInput"
+import { ModalTextInput } from "../shared/inputs/ModalTextInput"
+import { ModalNoteFileView } from "../shared/tiny/ModalNoteFileView"
+import { FaEye } from "react-icons/fa6"
+
+import styles from "./UpdateNoteForm.module.css"
+
+type UpdateNoteFormProps = {
+  note: FullNoteResponseData | null
+  handleSubmit: UseFormHandleSubmit<UpdateNoteFormFields>
+}
+
+export function UpdateNoteForm({ note, handleSubmit }: UpdateNoteFormProps): JSX.Element {
+  const [author, setAuthor] = useState<UserResponseData | null>(null)
+
+  const onSubmit = async (data: UpdateNoteFormFields) => {
+    alert(JSON.stringify(data, null, 2))
+  }
+
+  useEffect(() => {
+    const fetchAuthor = async (note: FullNoteResponseData) => {
+      const resp = await userService.getUserById(note.created_by_id)
+  
+      if (resp.success) {
+        setAuthor(resp.data)
+      } else {
+        alert(`Failed to fetch note author:\n${JSON.stringify(resp.errors, null, 2)}`)
+      }
+    }
+
+    if (note) fetchAuthor(note)
+  }, [note])
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <ModalActionRow>
+        <ModalSection
+          label={<ModalLabel
+            icon={<PiCrownFill color="#ada96dff" />}
+            title="Autor"
+          />}
+          input={<BaseModalTextInput disabled value={author?.username ?? "-"} />}
+        />
+        <ModalSection
+          label={<ModalLabel icon={<FaCalendarAlt color="#8ca1b4ff" />} title="Criação" />}
+          input={<BaseModalTextInput disabled value={getCreation(note?.created_at)} />}
+        />
+      </ModalActionRow>
+
+      <ModalActionRow>
+        <ModalSection
+          label={<ModalLabel icon={<FaEye color="#a085b3ff" />} title="Visibilidade" />}
+          input={<BaseModalTextInput disabled value={prettyVisibility(note?.visibility)} />}
+        />
+        <ModalSection
+          label={<ModalLabel icon={<FaCalendarAlt color="#8ca1b4ff" />} title="Última Atualização" />}
+          input={<BaseModalTextInput disabled value={getUpdate(note?.created_at, note?.updated_at)} />}
+        />
+      </ModalActionRow>
+
+      <ModalActionRow>
+        <ModalSection
+          label={<ModalLabel title="Conteúdo" required />}
+          input={<ModalNoteFileView note={note} />}
+        />
+      </ModalActionRow>
+
+      <div className={styles.division} />
+
+      <ModalActionRow>
+        <ModalSection
+          label={<ModalLabel title="Nome" required />}
+          input={<ModalTextInput name="name" />}
+        />
+      </ModalActionRow>
+
+      <ModalActionRow>
+        <ModalSection
+          label={<ModalLabel title="Tags" required={false} />}
+          input={<ModalArrayInput name="tags" minLength={2} maxLength={30} placeholder="Digite uma tag..." />}
+        />
+      </ModalActionRow>
+
+      <ModalFooter />
+    </form>
+  )
+}
+
+function prettyVisibility(visibility?: string): string {
+  if (!visibility) return '-'
+  return visibility === "PUBLIC" ? "Público" : "Confidencial"
+}
+
+function getUpdate(creation?: string, date?: string): string {
+  // For a simpler user experience, if the note was never updated,
+  // we keep this field empty (shows "-").
+  return !date || creation === date ? '-' : formatLocalTimestamp(date)
+}
+
+function getCreation(date?: string): string {
+  return date ? formatLocalTimestamp(date) : '-'
+}
--- src/components/modals/notes/updates/UpdateNoteModal.tsx
+++ src/components/modals/notes/updates/UpdateNoteModal.tsx
@@ -1,27 +1,14 @@
 import type { FullNoteResponseData } from "../../../../types/api/notes"
-import type { UserResponseData } from "../../../../types/api/users"
 import { useEffect, useRef, useState, type JSX } from "react"
 import { updateNoteSchema, type UpdateNoteFormFields } from "../../../../types/forms/notes"
 
 import { FormProvider, useForm } from "react-hook-form"
-import { ModalActionRow } from "../shared/sections/ModalActionRow"
-import { ModalSection } from "../shared/sections/ModalSection"
 import { noteService } from "../../../../services/noteService"
 import { ModalHeader } from "./ModalHeader"
 import { DarkWrapper } from "../../../DarkWrapper"
-import { ModalFooter } from "./ModalFooter"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { userService } from "../../../../services/userService"
 import { IoMdClose } from "react-icons/io"
-import { ModalLabel } from "../shared/sections/ModalLabel"
-import { BaseModalTextInput } from "../shared/inputs/BaseModalTextInput"
-import { formatLocalTimestamp } from "../../../../utils/utils"
-import { PiCrownFill } from "react-icons/pi"
-import { FaCalendarAlt } from "react-icons/fa"
-import { FaEye } from "react-icons/fa"
-import { ModalArrayInput } from "../shared/inputs/ModalArrayInput"
-import { ModalTextInput } from "../shared/inputs/ModalTextInput"
-import { ModalNoteFileView } from "../shared/tiny/ModalNoteFileView"
+import { UpdateNoteForm } from "./UpdateNoteForm"
 
 import styles from "./UpdateNoteModal.module.css"
 
@@ -32,7 +19,6 @@ type UpdateNoteModalProps = {
 
 export function UpdateNoteModal({ noteId, setIsPatching }: UpdateNoteModalProps): JSX.Element {
   const [note, setNote] = useState<FullNoteResponseData | null>(null)
-  const [author, setAuthor] = useState<UserResponseData | null>(null)
   const [isLoading, setIsLoading] = useState(false)
   const modalRef = useRef<HTMLDivElement>(null)
   const handleCloseModal = () => setIsPatching(false)
@@ -44,10 +30,6 @@ export function UpdateNoteModal({ noteId, setIsPatching }: UpdateNoteModalProps)
     }
   })
   const { handleSubmit, reset } = methods
-
-  const onSubmit = async (data: UpdateNoteFormFields) => {
-    alert(JSON.stringify(data, null, 2))
-  }
 
   useEffect(() => {
     const globalEscapeHandler = (e: KeyboardEvent) => {
@@ -62,16 +44,6 @@ export function UpdateNoteModal({ noteId, setIsPatching }: UpdateNoteModalProps)
   }, [setIsPatching])
 
   useEffect(() => {
-    const fetchAuthor = async (note: FullNoteResponseData) => {
-      const resp = await userService.getUserById(note.created_by_id)
-
-      if (resp.success) {
-        setAuthor(resp.data)
-      } else {
-        alert(`Failed to fetch note author:\n${JSON.stringify(resp.errors, null, 2)}`)
-      }
-    }
-
     const fetchNote = async () => {
       setIsLoading(true)
       const resp = await noteService.fetchNote(noteId)
@@ -79,7 +51,6 @@ export function UpdateNoteModal({ noteId, setIsPatching }: UpdateNoteModalProps)
 
       if (resp.success) {
         setNote(resp.data)
-        fetchAuthor(resp.data)
 
         reset({
           name: resp.data.name,
@@ -108,73 +79,8 @@ export function UpdateNoteModal({ noteId, setIsPatching }: UpdateNoteModalProps)
       <ModalHeader noteId={noteId} note={note} />
 
       <FormProvider {...methods}>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <ModalActionRow>
-            <ModalSection
-              label={<ModalLabel
-                icon={<PiCrownFill color="#ada96dff" />}
-                title="Autor"
-              />}
-              input={<BaseModalTextInput disabled value={author?.username ?? "-"} />}
-            />
-            <ModalSection
-              label={<ModalLabel icon={<FaCalendarAlt color="#8ca1b4ff" />} title="Criação" />}
-              input={<BaseModalTextInput disabled value={getCreation(note?.created_at)} />}
-            />
-          </ModalActionRow>
-
-          <ModalActionRow>
-            <ModalSection
-              label={<ModalLabel icon={<FaEye color="#a085b3ff" />} title="Visibilidade" />}
-              input={<BaseModalTextInput disabled value={prettyVisibility(note?.visibility)} />}
-            />
-            <ModalSection
-              label={<ModalLabel icon={<FaCalendarAlt color="#8ca1b4ff" />} title="Última Atualização" />}
-              input={<BaseModalTextInput disabled value={getUpdate(note?.created_at, note?.updated_at)} />}
-            />
-          </ModalActionRow>
-
-          <ModalActionRow>
-            <ModalSection
-              label={<ModalLabel title="Conteúdo" required />}
-              input={<ModalNoteFileView note={note} />}
-            />
-          </ModalActionRow>
-
-          <div className={styles.division} />
-
-          <ModalActionRow>
-            <ModalSection
-              label={<ModalLabel title="Nome" required />}
-              input={<ModalTextInput name="name" />}
-            />
-          </ModalActionRow>
-
-          <ModalActionRow>
-            <ModalSection
-              label={<ModalLabel title="Tags" required={false} />}
-              input={<ModalArrayInput name="tags" minLength={2} maxLength={30} placeholder="Digite uma tag..." />}
-            />
-          </ModalActionRow>
-
-          <ModalFooter />
-        </form>
+        <UpdateNoteForm note={note} handleSubmit={handleSubmit} />
       </FormProvider>
     </div>
   )
-}
-
-function prettyVisibility(visibility?: string): string {
-  if (!visibility) return '-'
-  return visibility === "PUBLIC" ? "Público" : "Confidencial"
-}
-
-function getUpdate(creation?: string, date?: string): string {
-  // For a simpler user experience, if the note was never updated,
-  // we keep this field empty (shows "-").
-  return !date || creation === date ? '-' : formatLocalTimestamp(date)
-}
-
-function getCreation(date?: string): string {
-  return date ? formatLocalTimestamp(date) : '-'
 }
