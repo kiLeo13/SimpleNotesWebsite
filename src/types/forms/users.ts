a27b087128fe55e05553b3eaafd67b314e0eba46
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-21 00:44:56 -0300

    Apply some internationalization to auth modals

--- src/types/forms/users.ts
+++ src/types/forms/users.ts
@@ -1,4 +1,8 @@
+import i18n from "../../services/i18n"
+
 import { z } from "zod"
+
+const t = i18n.t
 
 export const passwords = {
   minLength: 8,
@@ -6,21 +10,37 @@ export const passwords = {
 }
 
 export const loginSchema = z.object({
-  email: z.email(),
-  password: z.string().min(8).max(64)
+  email: z.email(t('errors.email.invalid')),
+
+  password: z
+    .string()
+    .min(8, t('errors.string.min', { count: 8 }))
+    .max(64, t('errors.string.max', { count: 64 }))
 })
 
 export const signupSchema = z.object({
-  username: z.string().min(2).max(80),
-  email: z.email(),
-  password: z.string().min(8).max(64)
+  username: z
+    .string()
+    .min(2, t('errors.string.min', { count: 2 }))
+    .max(80, t('errors.string.max', { count: 80 })),
+
+  email: z.email(t('errors.email.invalid')),
+
+  password: z
+    .string()
+    .min(8, t('errors.string.min', { count: 8 }))
+    .max(64, t('errors.string.max', { count: 64 }))
 })
 
 export const confirmSchema = z.object({
-  email: z.email(),
-  code: z.string().length(6)
+  email: z.email(t('errors.email.invalid')),
+
+  code: z
+    .string()
+    .length(6, t('errors.string.fixed', { val: 6 }))
 })
 
+// No i18n for this, since we don't use it for forms
 export const checkUserStatusSchema = z.object({
   email: z.email()
 })
