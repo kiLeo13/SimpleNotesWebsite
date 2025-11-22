9cb85733fb195272666d271108b39231a61f98f1
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-11-22 03:25:37 -0300

    Add more div `props` to the `LoaderContainer` component

--- src/components/LoaderContainer.tsx
+++ src/components/LoaderContainer.tsx
@@ -1,16 +1,16 @@
-import type { JSX } from "react"
+import type { ComponentProps, JSX } from "react"
 
 import clsx from "clsx"
 
 import styles from "./LoaderContainer.module.css"
 
-type LoaderContainerProps = {
+type LoaderContainerProps = ComponentProps<"div"> & {
   scale?: string
 }
 
-export function LoaderContainer({ scale }: LoaderContainerProps): JSX.Element {
+export function LoaderContainer({ scale, ...props }: LoaderContainerProps): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} {...props}>
       <div className={clsx("loader", styles.loader)} style={{ scale: scale }} />
     </div>
   )
