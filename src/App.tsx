f08a82f6cb492ac7b60d5e8f368f2e73398c048d
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-12-07 20:06:18 -0300

    Add `Toaster` component to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import { Toaster } from "sonner"
 import { MainPage } from "./pages/mainpage/MainPage"
 import { LoginPage } from "./pages/auth/LoginPage"
 import { SignupPage } from "./pages/auth/SignupPage"
@@ -10,22 +11,31 @@ import "./App.css"
 
 export const APP_NAME: string = "Onnyx"
 
-function App() {return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="login" element={<LoginPage />} />
-      <Route path="register" element={<SignupPage />} />
-
-      {/* Protected Routes */}
-      <Route
-        index
-        element={
-          <ProtectedRoute>
-            <MainPage />
-          </ProtectedRoute>
-        }
+function App() {
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        richColors
+        expand={true}
       />
-    </Routes>
+
+      <Routes>
+        {/* Public Routes */}
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<SignupPage />} />
+
+        {/* Protected Routes */}
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
   )
 }
 
