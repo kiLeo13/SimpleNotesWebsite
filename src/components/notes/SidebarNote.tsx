cc28561fd25d743baf3483d223e8f989e729a0b5
kiLeo13 <leonardo.silvavieira@hotmail.com>
2025-12-22 03:20:01 -0300

    Add open note hint on sidebar items

--- src/components/notes/SidebarNote.tsx
+++ src/components/notes/SidebarNote.tsx
@@ -1,9 +1,12 @@
 import type { NoteResponseData } from "@/types/api/notes"
 import { useState, type JSX, type MouseEventHandler } from "react"
 
+import clsx from "clsx"
+
 import { DarkWrapper } from "../DarkWrapper"
 import { IoMdSettings } from "react-icons/io"
 import { UpdateNoteModal } from "../modals/notes/updates/UpdateNoteModal"
+import { useNoteStore } from "@/stores/useNotesStore"
 
 import styles from "./SidebarNote.module.css"
 
@@ -15,6 +18,8 @@ type SidebarNoteProps = {
 
 export function SidebarNote({ note, onClick, isAdmin }: SidebarNoteProps): JSX.Element {
   const [isPatching, setIsPatching] = useState(false)
+  const shownNote = useNoteStore((state) => state.shownNote)
+  const isOpen = shownNote?.id === note.id
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
     e.stopPropagation()
@@ -22,7 +27,7 @@ export function SidebarNote({ note, onClick, isAdmin }: SidebarNoteProps): JSX.E
   }
 
   return (
-    <div onClick={onClick} className={styles.noteItem}>
+    <div onClick={onClick} className={clsx(styles.noteItem, isOpen && styles.open)}>
       <span className={styles.noteItemTitle}>{note.name}</span>
 
       {isAdmin && (
