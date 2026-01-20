7a3a0bc6e0213d01d8103845b579e1e7fda016a4
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-01-19 23:57:40 -0300

    Forward ref of `AppTooltip` component

--- src/components/ui/AppTooltip.tsx
+++ src/components/ui/AppTooltip.tsx
@@ -1,29 +1,35 @@
-import React, { type JSX } from "react"
+import React, { forwardRef } from "react"
 
 import * as Tooltip from "@radix-ui/react-tooltip"
 
 import styles from "./AppTooltip.module.css"
 
-interface AppTooltipProps {
+interface AppTooltipProps extends React.ComponentPropsWithoutRef<"button"> {
   children: React.ReactNode
   label: string
   delay?: number
   side?: "top" | "bottom" | "left" | "right"
 }
 
-export function AppTooltip({ children, label, delay = 100, side = "top" }: AppTooltipProps): JSX.Element {
-  return (
-    <Tooltip.Provider delayDuration={delay}>
-      <Tooltip.Root>
-        <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
+export const AppTooltip = forwardRef<HTMLButtonElement, AppTooltipProps>(
+  ({ children, label, delay = 100, side = "top", ...props }, ref) => {
+    return (
+      <Tooltip.Provider delayDuration={delay}>
+        <Tooltip.Root>
+          <Tooltip.Trigger asChild ref={ref} {...props}>
+            {children}
+          </Tooltip.Trigger>
 
-        <Tooltip.Portal>
-          <Tooltip.Content className={styles.tooltipContent} side={side} sideOffset={5}>
-            {label}
-            <Tooltip.Arrow className={styles.tooltipArrow} />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
-  )
-}
+          <Tooltip.Portal>
+            <Tooltip.Content className={styles.tooltipContent} side={side} sideOffset={5}>
+              {label}
+              <Tooltip.Arrow className={styles.tooltipArrow} />
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    )
+  }
+)
+
+AppTooltip.displayName = "AppTooltip"
