3ae359cf8dc8ebc20091e6bcfb98a7e71c9428e6
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-01-25 22:20:39 -0300

    Add `setUser` method to session store

--- src/stores/useSessionStore.ts
+++ src/stores/useSessionStore.ts
@@ -4,11 +4,17 @@ import { create } from "zustand"
 
 type SessionState = {
   user: UserResponseData | null
+
+  setUser: (user: UserResponseData) => void
   updateUser: (updates: Partial<UserResponseData>) => void
 }
 
 export const useSessionStore = create<SessionState>((set) => ({
   user: null,
+
+  setUser: (user) => {
+    set({ user: user })
+  },
 
   updateUser: (updates) => {
     set((state) => ({
