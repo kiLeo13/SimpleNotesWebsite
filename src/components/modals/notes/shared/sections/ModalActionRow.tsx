a00d3e3ec7316c10ae4e8f2154fd21b3823255bf
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-02-01 21:34:19 -0300

    Make `ModalActionRow` more flexible

--- src/components/modals/notes/shared/sections/ModalActionRow.tsx
+++ src/components/modals/notes/shared/sections/ModalActionRow.tsx
@@ -1,15 +1,17 @@
-import type { JSX } from "react"
+import type { ComponentProps, JSX } from "react"
+
+import clsx from "clsx"
 
 import styles from "./ModalActionRow.module.css"
 
-type ModalActionRowProps = {
-  children: React.ReactElement[] | React.ReactElement
-}
-
-export function ModalActionRow({ children: sections }: ModalActionRowProps): JSX.Element {
+export function ModalActionRow({
+  children,
+  className,
+  ...props
+}: ComponentProps<"div">): JSX.Element {
   return (
-    <div className={styles.modalActionRow}>
-      {sections}
+    <div className={clsx(styles.modalActionRow, className)} {...props}>
+      {children}
     </div>
   )
 }
