45a7d440d402456143ced1c5ed8eeca354bab1b1
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-02-26 09:47:51 -0300

    Make `Button` component `forwardRef`

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -1,6 +1,7 @@
 import {
   useEffect,
   useState,
+  forwardRef,
   type ComponentProps,
   type JSX,
   type CSSProperties
@@ -28,73 +29,81 @@ type ButtonProps = ComponentProps<"button"> & {
   cooldown?: number
 }
 
-export function Button({
-  isLoading,
-  loaderProps,
-  rippleProps,
-  cooldown = 0,
-  className,
-  children,
-  onClick,
-  disabled,
-  style,
-  ...props
-}: ButtonProps): JSX.Element {
-  const [isCoolingDown, setIsCoolingDown] = useState(cooldown > 0)
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      isLoading,
+      loaderProps,
+      rippleProps,
+      cooldown = 0,
+      className,
+      children,
+      onClick,
+      disabled,
+      style,
+      ...props
+    },
+    ref
+  ): JSX.Element => {
+    const [isCoolingDown, setIsCoolingDown] = useState(cooldown > 0)
 
-  useEffect(() => {
-    if (cooldown <= 0) return
+    useEffect(() => {
+      if (cooldown <= 0) return
 
-    setIsCoolingDown(true)
+      setIsCoolingDown(true)
 
-    const timerId = window.setTimeout(() => {
-      setIsCoolingDown(false)
-    }, cooldown)
+      const timerId = window.setTimeout(() => {
+        setIsCoolingDown(false)
+      }, cooldown)
 
-    return () => window.clearTimeout(timerId)
-  }, [cooldown])
+      return () => window.clearTimeout(timerId)
+    }, [cooldown])
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (disabled) return
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (disabled) return
 
-    if (isCoolingDown) {
-      e.preventDefault()
-      e.stopPropagation()
-      return
+      if (isCoolingDown) {
+        e.preventDefault()
+        e.stopPropagation()
+        return
+      }
+
+      onClick?.(e)
     }
 
-    onClick?.(e)
-  }
-
-  return (
-    <LoaderWrapper
-      className={styles.loaderWrapper}
-      isLoading={isLoading || false}
-      loaderProps={loaderProps}
-    >
-      <button
-        {...props}
-        onClick={handleClick}
-        disabled={disabled || isLoading}
-        tabIndex={isCoolingDown ? -1 : props.tabIndex}
-        style={
-          {
-            ...style,
-            "--cooldown-duration": `${cooldown}ms`
-          } as CustomCSS
-        }
-        className={clsx(
-          styles.button,
-          isCoolingDown && styles.coolingDown,
-          className
-        )}
+    return (
+      <LoaderWrapper
+        className={styles.loaderWrapper}
+        isLoading={isLoading || false}
+        loaderProps={loaderProps}
       >
-        {isCoolingDown && <span className={styles.cooldownBar} />}
+        <button
+          {...props}
+          ref={ref}
+          onClick={handleClick}
+          disabled={disabled || isLoading}
+          tabIndex={isCoolingDown ? -1 : props.tabIndex}
+          style={
+            {
+              ...style,
+              "--cooldown-duration": `${cooldown}ms`
+            } as CustomCSS
+          }
+          className={clsx(
+            styles.button,
+            isCoolingDown && styles.coolingDown,
+            className
+          )}
+        >
+          {isCoolingDown && <span className={styles.cooldownBar} />}
 
-        {!isCoolingDown && <Ripple {...rippleProps} />}
+          {!isCoolingDown && <Ripple {...rippleProps} />}
 
-        {children}
-      </button>
-    </LoaderWrapper>
-  )
-}
+          {children}
+        </button>
+      </LoaderWrapper>
+    )
+  }
+)
+
+Button.displayName = "Button"
