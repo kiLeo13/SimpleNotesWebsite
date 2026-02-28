79e4cf4de352bf5b028ee8455f9c737c509ae149
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-02-28 01:01:03 -0300

    Fix implementation of `logout` at session store

--- src/stores/useSessionStore.ts
+++ src/stores/useSessionStore.ts
@@ -7,21 +7,37 @@ type SessionState = {
   user: UserResponseData | null
 
   getIdToken: () => string | null
+  getAccessToken: () => string | null
+
   logout: () => void
   setUser: (user: UserResponseData) => void
   updateUser: (updates: Partial<UserResponseData>) => void
 }
 
-export const useSessionStore = create<SessionState>((set) => ({
+export const useSessionStore = create<SessionState>((set, get) => ({
   user: null,
 
   getIdToken() {
     return localStorage.getItem("id_token")
   },
 
-  // This is a FF (Fire and Forget) method.
-  // This function checks nothing about the server response.
-  logout: () => userService.logout(),
+  getAccessToken() {
+    return localStorage.getItem("access_token")
+  },
+
+  /**
+   * Fire-and-forget logout.
+   *
+   * Sends a logout request if an `accessToken` exists.
+   * Does not check the response or handle errors.
+   * Silently returns if no token is available.
+   */
+  logout: () => {
+    const accessToken = get().getAccessToken()
+    if (!accessToken) return
+
+    userService.logout({ access_token: accessToken })
+  },
 
   setUser: (user) => {
     set({ user: user })
