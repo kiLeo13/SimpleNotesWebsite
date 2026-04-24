1ff19e7a916dd73b5137f3da483bfa8feed13c0b
kiLeo13 <leonardo.silvavieira@hotmail.com>
2026-04-24 00:04:35 -0300

    Format connection shim file

--- backend/infrastructure/aws/lambda/ws-connect-shim/index.mjs
+++ backend/infrastructure/aws/lambda/ws-connect-shim/index.mjs
@@ -1,44 +1,44 @@
 export const handler = async (event) => {
-    const backendUrl = process.env.BACKEND_URL
-    const connectionId = event.requestContext.connectionId
-    const token = (event.queryStringParameters || {}).token || ""
-    const sessionId = (event.queryStringParameters || {}).session_id || ""
+  const backendUrl = process.env.BACKEND_URL;
+  const connectionId = event.requestContext.connectionId;
+  const token = (event.queryStringParameters || {}).token || "";
+  const sessionId = (event.queryStringParameters || {}).session_id || "";
 
-    if (!sessionId) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: "session_id is required"
-            })
-        }
-    }
+  if (!sessionId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "session_id is required",
+      }),
+    };
+  }
 
-    const headers = {
-        "Content-Type": "application/json",
-        "X-Connection-Id": connectionId,
-        "X-Session-Id": sessionId,
-        "Authorization": token ? `Bearer ${token}` : ""
-    }
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Connection-Id": connectionId,
+    "X-Session-Id": sessionId,
+    Authorization: token ? `Bearer ${token}` : "",
+  };
 
-    try {
-        const response = await fetch(backendUrl, {
-            method: "POST",
-            headers: headers
-        })
+  try {
+    const response = await fetch(backendUrl, {
+      method: "POST",
+      headers: headers,
+    });
 
-        const responseBody = await response.text()
+    const responseBody = await response.text();
 
-        return {
-            statusCode: response.status,
-            body: responseBody
-        }
-    } catch (error) {
-        console.error("Shim Error:", error)
-        return {
-            statusCode: 502,
-            body: JSON.stringify({
-                "message": "Bad Gateway: Backend Unreachable"
-            })
-        }
-    }
-}
+    return {
+      statusCode: response.status,
+      body: responseBody,
+    };
+  } catch (error) {
+    console.error("Shim Error:", error);
+    return {
+      statusCode: 502,
+      body: JSON.stringify({
+        message: "Bad Gateway: Backend Unreachable",
+      }),
+    };
+  }
+};
